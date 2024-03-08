6da2d6b257d8d7a68e1fcd6767d9fe62deebf0d6
Jay <me@jayxtq.xyz>
2024-03-08 21:21:34 +0000

    feat: add invite command

---
+++ src/commands/invite.ts
@@ -0,0 +1,9 @@
+import { Command } from "../types";
+
+export default {
+    name: "invite",
+    description: "Get the bot's invite link",
+    run: async (interaction) => {
+        interaction.reply({ content: "https://discord.com/oauth2/authorize?client_id=1215749607657836606&permissions=18432&scope=bot+applications.commands", ephemeral: true });
+    },
+} as Command;
