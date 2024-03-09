44375e5acf350aab527be2215ce70c9d2409b7ea
rad <me@radnotred.dev>
2024-03-08 21:36:47 -0500

    chore: use env instead of set id

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -4,6 +4,9 @@ export default {
     name: "invite",
     description: "Get the bot's invite link",
     run: async (interaction) => {
-        interaction.reply({ content: "https://discord.com/oauth2/authorize?client_id=1215749607657836606&permissions=18432&scope=bot+applications.commands", ephemeral: true });
+        await interaction.reply({
+            content: `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=18432&scope=bot+applications.commands`,
+            ephemeral: true
+        });
     },
 } satisfies Command;
